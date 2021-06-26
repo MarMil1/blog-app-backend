cfdfa44d3d76d62aceab85a04d1eec22c2264bb6
MarMil1 <milosavljevic.marko@outlook.com>
2021-06-25 21:33:11 -0500

    Connecting frontend UI.

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const userRoute = require('./routes/users.route');
 const articleRoute = require('./routes/articles.route');
 const categoryRoute = require('./routes/categories.route');
 const multer = require('multer');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
